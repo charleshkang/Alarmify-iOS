2d8d1deffc0c43355205b757fdda2026b1ec6e13
charles <charleshkang1@gmail.com>
2016-02-11 18:09:39 -0500

    first commit

---
+++ Alarmify/Alarmify/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Alarmify
+//
+//  Created by Charles Kang on 2/11/16.
+//  Copyright © 2016 Charles Kang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
